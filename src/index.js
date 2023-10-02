62cf84049d74aa197fc50ed3ca8e8d8fade8f1ef
Konan <mastercelis8891@gmail.com>
2023-10-02 10:37:53 -0400

    ready to deploy part 2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App'
+import App from './components/App';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+
+if(process.env.NODE_ENV === "production") {
+    disableReactDevTools();
+}
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
